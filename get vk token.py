5c8ee72e1b81f1e132625410fe0897fad99a4177
Sergei Romanov <l2omanov@ya.ru>
2020-08-20 14:03:36 +0400

    smth fixed

---
+++ get vk token.py
@@ -0,0 +1,15 @@
+from urllib.parse import urlencode
+
+app_id = 7542017
+oauth_url = 'https://oauth.vk.com/authorize'
+oauth_data = {
+    'client_id': app_id,
+    'display': 'page',
+    'scope': 'status, audio, friends, photos, video',
+    'response_type': 'token',
+    'v': 5.12
+}
+
+print('?'.join(
+    (oauth_url, urlencode(oauth_data))
+))
